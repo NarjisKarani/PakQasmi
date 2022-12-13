c8746287eca7e17d177b25504ef0d008f5db2a21
Narjis Karani <narjiskarani@gmail.com>
2022-12-13 19:41:42 +0500

    model schema

--- backend/models/members.js
+++ backend/models/members.js
@@ -2,30 +2,35 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const {isEmail} = require('validator');
 
-const MemberSchema = new Schema({
-    name: {type: String},
-    fathername: {type: String},
-    gender: {type: String},
-    email:    {type: String, validate: [isEmail, 'Enter a valid email address'], lowercase: true,},
-    group: {type: String},
-    dob: {type: Date},
-    mobilenumber: {type: Number},
-    registrationnumber: {type: Number},
-    cnic: {type: Number},
-    address: {type: String},
-    city: {type: String, default: 'Karachi'},
-    occupation: {type: String},
-    education: {type: String},
-    bloodgroup: {type: String},
-    emergencycontact: {type: Number},
-    emergency: {type: String},
-    // image: {type: String, desc: String,
-    //     img:
-    //     {
-    //         data: Buffer,
-    //         contentType: String
-    //     }},
-    // documents: {type: String},
-});
+const MemberSchema = new Schema(
+    {
+        name: {type: String, required: true},
+        fathername: {type: String, required: true},
+        gender: {type: String, required: true},
+        email:    {type: String, required: true, validate: [isEmail, 'Enter a valid email address'], lowercase: true,},
+        group: {type: String, required: true},
+        dob: {type: Date, required: true},
+        mobilenumber: {type: Number, required: true},
+        registrationnumber: {type: Number, required: true},
+        cnic: {type: Number, required: true},
+        address: {type: String, required: true},
+        city: {type: String, default: 'Karachi', required: true},
+        occupation: {type: String, required: true},
+        education: {type: String, required: true},
+        bloodgroup: {type: String, required: true},
+        emergencycontact: {type: Number},
+        emergency: {type: String},
+        // image: {type: String, desc: String,
+        //     img:
+        //     {
+        //         data: Buffer,
+        //         contentType: String
+        //     }},
+        // documents: {type: String},
+    },
+    {
+        timestamps: true,
+    }
+);
 
 module.exports = mongoose.model('Members', MemberSchema);
